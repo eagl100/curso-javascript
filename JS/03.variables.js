6aa811548379aff54020d79eee0e2332b82bc828
eagl100 <exequielantonio.gallardo@alumnos.ulagos.cl>
2024-08-28 19:40:15 -0400

    Añadir archivo JS variables

---
+++ JS/03.variables.js
@@ -0,0 +1,23 @@
+//variables que pueden cambiar en el tiempo
+var estatura =1.71
+let peso = 60
+
+//
+const nombre = "Exequiel"
+
+//declaracion de variable
+var edad
+let ciudad
+
+//declaración e inicialización de variables
+edad = 22
+ciudad = "Osorno"
+
+const apellido = "Gallardo"
+//Concatenando variables de tipo String
+const nombrecompleto = nombre + " " + apellido
+console.log(nombrecompleto);
+
+//declarando e inicializando variables en una linea
+//Uso incorrecto de declaración de variables
+let docente = 'Tomas', age = 25, asignaturas = ['IA','Seguridad Informatica'];
