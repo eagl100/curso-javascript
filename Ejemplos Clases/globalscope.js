328bacfc5def8bc059bed0e32d23a23b07a7230b
eagl100 <exequielantonio.gallardo@alumnos.ulagos.cl>
2024-09-10 19:44:41 -0300

    Subida de archivo globalscope.js

---
+++ Ejemplos Clases/globalscope.js
@@ -0,0 +1,11 @@
+var comic = "X-Men"
+
+function comprar(){
+    console.log(`Compraste el comic ${(comic)}`)
+}
+// Compraste el comic X-Men
+
+function leer(){
+    console.log(`Estas leyendo ${(comic)}`)
+}
+// Estas leyendo X-Men
