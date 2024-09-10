23d9d650b6bedeae30f30c31ae5f67bcf33d12e4
eagl100 <exequielantonio.gallardo@alumnos.ulagos.cl>
2024-09-10 19:45:16 -0300

    Subida de archivo functionscope.js

---
+++ Ejemplos Clases/functionscope.js
@@ -0,0 +1,13 @@
+function justiceLeague(){
+    var heroes = [`Batman`,`Superman`,`Aquaman`]
+    console.log(heroes)
+    //[`Batman`,`Superman`,`Aquaman`]
+}
+function teenTitans(){
+    var heroes = [`Robien`,`Raven`,`Terra`]
+    console.log(heroes)
+    //[`Robien`,`Raven`,`Terra`]
+}
+console.log(heroes)
+// Reference error
+// heroes is not defined
