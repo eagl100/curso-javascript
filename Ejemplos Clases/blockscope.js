bb7679cea18e1776c538c46bc8a05c4ab4e03566
eagl100 <exequielantonio.gallardo@alumnos.ulagos.cl>
2024-09-10 19:44:57 -0300

    Subida de archivo blockscope.js

---
+++ Ejemplos Clases/blockscope.js
@@ -0,0 +1,12 @@
+function mutantes(){
+    const mutante = `Wolverine`
+    if(true){
+    //Esta variable se quedara solo
+    //Entre estas llaves
+    const mutante = `Ciclope`
+    console.log(mutante)
+    //Ciclope 
+    }
+    console.log(mutante)
+    //Wolverine
+}
